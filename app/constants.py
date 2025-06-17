b59ed176b60dd23291ff8589feac612dc76fcf25
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-06-17 05:33:30 +0500

    Add CORS and session middleware to FastAPI application

---
+++ app/constants.py
@@ -0,0 +1,4 @@
+ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+    "http://localhost:3000/",
+]
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from fyp-backend!")
+import logging
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from app.constants import ALLOWED_ORIGINS
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+load_dotenv(override=True)
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    # # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SESSION_SECRET_KEY")
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"FYP Backend": "Online 👍"}
