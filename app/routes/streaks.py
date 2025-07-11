6c92093b217e04784777ea22e399152e0afc848b
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-11 07:02:08 +0500

    update streaks.py

--- app/routes/streaks.py
+++ app/routes/streaks.py
@@ -1,22 +1,30 @@
-from fastapi import Depends, APIRouter, HTTPException, status
+import json
+from fastapi import Depends, APIRouter, HTTPException, Response, status
 from app.auth.dependencies import get_current_user
 from app.database.connection import PostgresConnection
 from app.database.streaks import get_user_streak, update_user_streak
 
 router = APIRouter(prefix="/streak", tags=["Streaks"])
 
+
 @router.post("/update", status_code=status.HTTP_201_CREATED)
 def update_streak(user_id: str = Depends(get_current_user)):
     try:
         with PostgresConnection() as conn:
             result = update_user_streak(conn, user_id)
-            print("🐍 File: routes/streaks.py | Line: 13 | update_streak ~ result",result)
-            return {
-                "current_streak": result["current_streak"],
-                "updated": result["updated"]
-            }
+            if not result["updated"]:
+                return Response(
+                    status_code=200,
+                    content=json.dumps(
+                        {
+                            "current_streak": result["current_streak"],
+                            "updated": result["updated"],
+                        }
+                    ),
+                )
     except Exception as e:
         return HTTPException(status_code=500, detail="Failed to update streak")
+
 
 @router.get("", status_code=status.HTTP_200_OK)
 def get_user_streak_status(user_id: str = Depends(get_current_user)):
