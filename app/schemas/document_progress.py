2256d640f8b06ac2b3e7687bad283733f351d23b
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-11 07:46:20 +0500

    update document_progress.py

--- app/schemas/document_progress.py
+++ app/schemas/document_progress.py
@@ -6,5 +6,5 @@ class DocumentProgressUpdate(BaseModel):
     document_id: UUID = Field(..., )
     document_type: str = Field(..., example="book")
     page_number: Optional[int] = Field(None, example=12)
-    section_id: Optional[UUID] = Field(None, example="id or Can be null as well")
-    chapter_id: Optional[UUID] = Field(None, example="id or Can be null as well")
+    section_id: Optional[UUID] = Field(None, example="id or dont send if not needed")
+    chapter_id: Optional[UUID] = Field(None, example="id or dont send if not needed")
