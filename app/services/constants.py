c00485642f06429efa3237783445c4dc52480c17
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-14 06:58:41 +0500

    Add deepseek config

--- app/services/constants.py
+++ app/services/constants.py
@@ -13,8 +13,8 @@ SERVICE_CONFIG = {
         "base_url": "https://router.huggingface.co/featherless-ai/v1",
         "api_key_env_var": "HF_TOKEN",
     },
-    # "deepseek": {
-    #     "base_url": "https://api.deepseek.com",
-    #     "api_key_env_var": "DEEPSEEK_API_KEY",
-    # }
+    "deepseek": {
+        "base_url": "https://api.deepseek.com",
+        "api_key_env_var": "DEEPSEEK_API_KEY",
+    }
 }
