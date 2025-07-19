32bd67250cd1bedce9142c28a2ff4de85b48119b
Abdullah Naseem <abdullahnaseem27@gmail.com>
2025-07-19 05:39:57 +0500

    feat: add Kimi-K2 model constants and update Hugging Face service configuration

--- app/services/constants.py
+++ app/services/constants.py
@@ -7,13 +7,15 @@ USER_ROLE = "user"
 ASSISTANT_ROLE = "assistant"
 DEFAULT_MODEL_ID = "d50a33ce-2462-4a5a-9aa7-efc2d1749745" # LLAMA_3_70b
 # DEFAULT_MODEL_ID = "a1b6e28b-6c75-4695-be78-ce8fa3c11c06" # DeepSeek Chat
+KIMI_K2_INSTRUCT = "moonshotai/Kimi-K2-Instruct:groq"
+KIMI_K2_INSTRUCT_ID ="0068ad8d-ff21-4633-8c8e-6bb0282e0d82" # Kimi-K2-Instruct
 
 SERVICE_CONFIG = {
     "groq": {
         "base_url": "https://api.groq.com/openai/v1",
         "api_key_env_var": "GROQ_API_KEY",
     },
-     "huggingface": {
+     "huggingface_featherless": {
         "base_url": "https://router.huggingface.co/featherless-ai/v1",
         "api_key_env_var": "HF_TOKEN",
     },
@@ -21,7 +23,10 @@ SERVICE_CONFIG = {
         "base_url": "https://router.huggingface.co/hyperbolic/v1",
         "api_key_env_var": "HF_TOKEN",
     },
-     
+     "huggingface": {
+        "base_url": "https://router.huggingface.co/v1",
+        "api_key_env_var": "HF_TOKEN",
+    },
     "deepseek": {
         "base_url": "https://api.deepseek.com",
         "api_key_env_var": "DEEPSEEK_API_KEY",
